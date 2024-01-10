19436d63b7d562b90c4988aeb0ee9a8f19dbc6a4
Jorri Fransen <jorri@jorrifransen.nl>
2024-01-10 18:20:19 +0100

    More build targets on windows.

--- create_builds.bat
+++ create_builds.bat
@@ -1,2 +1,10 @@
 rmdir /s /q build
-meson setup build -Dunity=off
+rmdir /s /q build-vs
+rmdir /s /q build-clang
+
+meson setup --unity off build
+meson setup --unity off build-vs --backend vs
+
+set CC=clang-cl
+set CXX=clang-cl
+meson setup  --unity off build-clang
