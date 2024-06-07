2869fbf918ed43a8f5d575fbfc65624136974f97
Jorri Fransen <jorri@jorrifransen.nl>
2024-06-07 23:07:17 +0200

    Adding nix shell.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with (import <nixpkgs> {});
+
+  pkgs.mkShell {
+    name = "novo-dev";
+    hardeningDisable = [ "all" ];
+    nativeBuildInputs = with pkgs; [
+      clang-tools
+      clang
+      gdb
+      meson
+      ninja
+    ];
+    buildInputs = [];
+  }
